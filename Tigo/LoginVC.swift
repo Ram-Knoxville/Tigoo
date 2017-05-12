b1fa8da79a20caa17ff3604ffb8783daea7d0362
Ram-Knoxville <raguirre8911@gmail.com>
2017-05-12 18:16:26 -0500

    added video background on login

--- Tigo/LoginVC.swift
+++ Tigo/LoginVC.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class LoginVC: UIViewController {
 
     @IBOutlet weak var menuBtn: UIBarButtonItem!
     
+    
+    var player: AVPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +22,34 @@ class LoginVC: UIViewController {
         menuBtn.target = SWRevealViewController()
         
         menuBtn.action = #selector(SWRevealViewController.revealToggle(_:))
+        
 
+        let videoURL: NSURL = Bundle.main.url(forResource: "tireAnimation", withExtension: "mp4")! as NSURL
+        
+        player = AVPlayer(url: videoURL as URL)
+        player?.actionAtItemEnd = .none
+        player?.isMuted = true
+        
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        playerLayer.zPosition = -1
+        
+        playerLayer.frame = view.frame
+        
+        view.layer.addSublayer(playerLayer)
+        
+        player?.play()
+        
+        //loop video
+        NotificationCenter.default.addObserver(self,
+                                                         selector: #selector(LoginVC.loopVideo),
+                                                         name: NSNotification.Name.AVPlayerItemDidPlayToEndTime,
+                                                         object: nil)
+    }
+    
+    func loopVideo() {
+        player?.seek(to: kCMTimeZero)
+        player?.play()
     }
 
 }
