b17fe48bd8cc3ba8913131ac59d026e0d6839dbb
Ram-Knoxville <raguirre8911@gmail.com>
2017-05-12 16:08:22 -0500

    Improved Menu View

--- Tigo/MenuVC.swift
+++ Tigo/MenuVC.swift
@@ -11,6 +11,7 @@ import Font_Awesome_Swift
 
 class MenuVC: UIViewController {
 
+    // Iconos de Menú
     @IBOutlet weak var iconoInicio: UIImageView!
     @IBOutlet weak var iconoCuenta: UIImageView!
     @IBOutlet weak var iconoCarrito: UIImageView!
@@ -22,10 +23,34 @@ class MenuVC: UIViewController {
     @IBOutlet weak var iconoBuscar: UIImageView!
     @IBOutlet weak var iconoLogin: UIImageView!
     
+    //Labels de Menu
+    @IBOutlet weak var labelInicio: UILabel!
+    @IBOutlet weak var labelCuenta: UILabel!
+    @IBOutlet weak var labelCarrito: UILabel!
+    @IBOutlet weak var labelPedidos: UILabel!
+    @IBOutlet weak var labelDeseos: UILabel!
+    @IBOutlet weak var labelCategorias: UILabel!
+    @IBOutlet weak var labelPromociones: UILabel!
+    @IBOutlet weak var labelContacto: UILabel!
+    @IBOutlet weak var labelBuscar: UILabel!
+    @IBOutlet weak var labelLogin: UILabel!
+    
+    // Celdas Custom UIView
+    @IBOutlet weak var vistaInicio: UIView!
+    @IBOutlet weak var vistaMiCuenta: UIView!
+    @IBOutlet weak var vistaMiCarrito: UIView!
+    @IBOutlet weak var vistaMisPedidos: UIView!
+    @IBOutlet weak var vistaListaDeseos: UIView!
+    @IBOutlet weak var vistaCategorias: UIView!
+    @IBOutlet weak var vistaPromociones: UIView!
+    @IBOutlet weak var vistaContacto: UIView!
+    @IBOutlet weak var vistaBuscar: UIView!
+    @IBOutlet weak var vistaLogin: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Seteo de iconos a imagenes
         iconoInicio.setFAIconWithName(icon: .FAHome, textColor: .darkGray)
         iconoCuenta.setFAIconWithName(icon: .FAUser, textColor: .darkGray)
         iconoCarrito.setFAIconWithName(icon: .FAShoppingCart, textColor: .darkGray)
@@ -36,6 +61,131 @@ class MenuVC: UIViewController {
         iconoContacto.setFAIconWithName(icon: .FAMobilePhone, textColor: .darkGray)
         iconoBuscar.setFAIconWithName(icon: .FASearch, textColor: .darkGray)
         iconoLogin.setFAIconWithName(icon: .FAUserCircleO, textColor: .darkGray)
+        
     }
-
+    
+    @IBAction func InicioTapped(_ sender: Any) {
+        vistaInicio.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        vistaMiCuenta.backgroundColor = UIColor.white
+        vistaMiCarrito.backgroundColor = UIColor.white
+        vistaMisPedidos.backgroundColor = UIColor.white
+        vistaListaDeseos.backgroundColor = UIColor.white
+        vistaCategorias.backgroundColor = UIColor.white
+        vistaPromociones.backgroundColor = UIColor.white
+        vistaContacto.backgroundColor = UIColor.white
+        vistaBuscar.backgroundColor = UIColor.white
+        vistaLogin.backgroundColor = UIColor.white
+    }
+    
+    @IBAction func cuentaTapped(_ sender: Any) {
+        vistaInicio.backgroundColor = UIColor.white
+        vistaMiCuenta.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        vistaMiCarrito.backgroundColor = UIColor.white
+        vistaMisPedidos.backgroundColor = UIColor.white
+        vistaListaDeseos.backgroundColor = UIColor.white
+        vistaCategorias.backgroundColor = UIColor.white
+        vistaPromociones.backgroundColor = UIColor.white
+        vistaContacto.backgroundColor = UIColor.white
+        vistaBuscar.backgroundColor = UIColor.white
+        vistaLogin.backgroundColor = UIColor.white
+    }
+    @IBAction func carritoTapped(_ sender: Any) {
+        vistaInicio.backgroundColor = UIColor.white
+        vistaMiCuenta.backgroundColor = UIColor.white
+        vistaMiCarrito.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        vistaMisPedidos.backgroundColor = UIColor.white
+        vistaListaDeseos.backgroundColor = UIColor.white
+        vistaCategorias.backgroundColor = UIColor.white
+        vistaPromociones.backgroundColor = UIColor.white
+        vistaContacto.backgroundColor = UIColor.white
+        vistaBuscar.backgroundColor = UIColor.white
+        vistaLogin.backgroundColor = UIColor.white
+    }
+    @IBAction func pedidosTapped(_ sender: Any) {
+        vistaInicio.backgroundColor = UIColor.white
+        vistaMiCuenta.backgroundColor = UIColor.white
+        vistaMiCarrito.backgroundColor = UIColor.white
+        vistaMisPedidos.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        vistaListaDeseos.backgroundColor = UIColor.white
+        vistaCategorias.backgroundColor = UIColor.white
+        vistaPromociones.backgroundColor = UIColor.white
+        vistaContacto.backgroundColor = UIColor.white
+        vistaBuscar.backgroundColor = UIColor.white
+        vistaLogin.backgroundColor = UIColor.white
+    }
+    @IBAction func deseosTapped(_ sender: Any) {
+        vistaInicio.backgroundColor = UIColor.white
+        vistaMiCuenta.backgroundColor = UIColor.white
+        vistaMiCarrito.backgroundColor = UIColor.white
+        vistaMisPedidos.backgroundColor = UIColor.white
+        vistaListaDeseos.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        vistaCategorias.backgroundColor = UIColor.white
+        vistaPromociones.backgroundColor = UIColor.white
+        vistaContacto.backgroundColor = UIColor.white
+        vistaBuscar.backgroundColor = UIColor.white
+        vistaLogin.backgroundColor = UIColor.white
+    }
+    @IBAction func categoriasTapped(_ sender: Any) {
+        vistaInicio.backgroundColor = UIColor.white
+        vistaMiCuenta.backgroundColor = UIColor.white
+        vistaMiCarrito.backgroundColor = UIColor.white
+        vistaMisPedidos.backgroundColor = UIColor.white
+        vistaListaDeseos.backgroundColor = UIColor.white
+        vistaCategorias.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        vistaPromociones.backgroundColor = UIColor.white
+        vistaContacto.backgroundColor = UIColor.white
+        vistaBuscar.backgroundColor = UIColor.white
+        vistaLogin.backgroundColor = UIColor.white
+    }
+    @IBAction func promocionesTapped(_ sender: Any) {
+        vistaInicio.backgroundColor = UIColor.white
+        vistaMiCuenta.backgroundColor = UIColor.white
+        vistaMiCarrito.backgroundColor = UIColor.white
+        vistaMisPedidos.backgroundColor = UIColor.white
+        vistaListaDeseos.backgroundColor = UIColor.white
+        vistaCategorias.backgroundColor = UIColor.white
+        vistaPromociones.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        vistaContacto.backgroundColor = UIColor.white
+        vistaBuscar.backgroundColor = UIColor.white
+        vistaLogin.backgroundColor = UIColor.white
+    }
+    @IBAction func contactoTapped(_ sender: Any) {
+        vistaInicio.backgroundColor = UIColor.white
+        vistaMiCuenta.backgroundColor = UIColor.white
+        vistaMiCarrito.backgroundColor = UIColor.white
+        vistaMisPedidos.backgroundColor = UIColor.white
+        vistaListaDeseos.backgroundColor = UIColor.white
+        vistaCategorias.backgroundColor = UIColor.white
+        vistaPromociones.backgroundColor = UIColor.white
+        vistaContacto.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        vistaBuscar.backgroundColor = UIColor.white
+        vistaLogin.backgroundColor = UIColor.white
+    }
+    @IBAction func buscarTapped(_ sender: Any) {
+        vistaInicio.backgroundColor = UIColor.white
+        vistaMiCuenta.backgroundColor = UIColor.white
+        vistaMiCarrito.backgroundColor = UIColor.white
+        vistaMisPedidos.backgroundColor = UIColor.white
+        vistaListaDeseos.backgroundColor = UIColor.white
+        vistaCategorias.backgroundColor = UIColor.white
+        vistaPromociones.backgroundColor = UIColor.white
+        vistaContacto.backgroundColor = UIColor.white
+        vistaBuscar.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        vistaLogin.backgroundColor = UIColor.white
+    }
+    @IBAction func loginTapped(_ sender: Any) {
+        vistaInicio.backgroundColor = UIColor.white
+        vistaMiCuenta.backgroundColor = UIColor.white
+        vistaMiCarrito.backgroundColor = UIColor.white
+        vistaMisPedidos.backgroundColor = UIColor.white
+        vistaListaDeseos.backgroundColor = UIColor.white
+        vistaCategorias.backgroundColor = UIColor.white
+        vistaPromociones.backgroundColor = UIColor.white
+        vistaContacto.backgroundColor = UIColor.white
+        vistaBuscar.backgroundColor = UIColor.white
+        vistaLogin.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+    }
+    
+    
+    
 }
